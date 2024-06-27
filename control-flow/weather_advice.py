83096e24f449d747bfc7bdfb0163c5399cafa7b6
Betsy Rotich <betsyrotich022@gmail.com>
2024-06-27 22:20:38 +0300

    Weather Recommendation Program

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,6 +1,6 @@
 current_weather = input("What's the weather like today? (sunny/rainy/cold):")
 if current_weather == "sunny":
-   print("Wear a t-shirt and sunglasses.", current_weather)
+   print("Wear a t-shirt and sunglasses", current_weather)
 elif current_weather == "rainy":
    print("Don't forget your umbrella and a raincoat.", current_weather)
 elif current_weather == "cold":
