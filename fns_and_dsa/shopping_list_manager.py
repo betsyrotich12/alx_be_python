ea69a481513368fe656445f7f8c48eaff10c58ff
Betsy Rotich <betsyrotich022@gmail.com>
2024-07-05 19:06:38 +0300

    Shopping List Manager

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,9 +1,9 @@
 shopping_list = []
 def display_menu():
      print("Shopping List Manager")
-     print("1. Add an item")
-     print("2. Remove an item")
-     print("3. View the list")
+     print("1. Add Item")
+     print("2. Remove Item")
+     print("3. View List")
      print("4. Exit")
 
 
