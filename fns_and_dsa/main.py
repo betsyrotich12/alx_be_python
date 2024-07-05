6a78b46963135f244078c4e2a25d293d82c3d946
Betsy Rotich <betsyrotich022@gmail.com>
2024-07-05 19:50:11 +0300

    Shopping List Manager

--- fns_and_dsa/main.py
+++ fns_and_dsa/main.py
@@ -15,7 +15,7 @@ def main():
         if choice == '1':
             # Prompt for and add an item
             item = input("Enter the item to add: ")
-            shopping_list.append(item)
+            add_item(shopping_list)
             pass
         elif choice == '2':
             # Prompt for and remove an item
