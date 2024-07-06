3b3c5beffd83c1600a974aeec0e423dda1c9b8bb
Betsy Rotich <betsyrotich022@gmail.com>
2024-07-06 11:51:11 +0300

    Temperature Conversion Tool

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,5 +1,10 @@
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+try:
+    temperature = float(input("Enter the temperature to convert:"))
+except ValueError:
+        raise ValueError("Invalid temperature. Please enter a numeric value.")
+unit = input("Is this temperature in Celsius or Farenheit? (C/F):")
 
 def convert_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
