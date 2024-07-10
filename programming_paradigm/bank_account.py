9612a9e3e33a686083d100ccdf261bba2fde88e4
Betsy Rotich <betsyrotich022@gmail.com>
2024-07-10 19:13:21 +0300

    Simple Bank Account Class

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -17,4 +17,4 @@ class BankAccount:
 
 
     def display_balance(self):
-        print(f"Current Balance: $ {self.account_balance:.2f}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
